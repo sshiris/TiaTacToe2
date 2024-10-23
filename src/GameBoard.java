9525c067cc72dfa49f2cbdfeb2ab07d8780ea94a
Iris <148431646+iris915520@users.noreply.github.com>
2024-10-23 11:26:39 +0300

    break APP.java into three classes

---
+++ src/GameBoard.java
@@ -0,0 +1,64 @@
+public class GameBoard {
+    char symbol = ' ';
+    char[][] gameBoard;
+
+    public GameBoard() {
+        gameBoard = new char[][] {
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' },
+        };
+    }
+
+    public void printGameBoard() {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public void placePiece(int pos, User user) {
+        if (user.competitor.equals("player")) {
+            symbol = 'X';
+        } else if (user.competitor.equals("cpu")) {
+            symbol = 'O';
+        }
+        user.positions.add(pos);
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                break;
+        }
+
+    }
+}
